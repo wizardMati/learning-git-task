d8dfb2269608c45712a59eff851180142f3011e1
Mateusz Monikowski <mateo.monikowski@gmail.com>
2025-10-20 13:45:14 +0200

    Add shoping list dictinary

--- zad3.2.py
+++ zad3.2.py
@@ -1,4 +1,13 @@
 shopping = {
-    "piekarnia":["chleb","kajzerka","paczek",],
-    "warzywniak":["brokul","kalafior","pomidor","roszponka"]
+    "piekarnia": ["chleb", "pączek", "bułki"],
+    "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+for shop, products in shopping.items():
+    capitalized_products = [item.capitalize() for item in products]
+    print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {capitalized_products}.")
+
+total_items = sum(len(products) for products in shopping.values())
+print(f"W sumie kupuję {total_items} produktówe.")
+
+
